3ab5dd53e42dc0dd0b9d2601799771bc5d22c373
paldevopspaldevops <paldevops92@gmail.com>
2024-04-12 23:51:21 +0530

    hello hi

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,7 +2,7 @@
 PERSON1=pal
 PERSON2=pappa
 
-echo"$PERSON1::hello good morning"
-echo"$PERSON2::hello $PERSON2 good morning"
-echo"$PERSON1::$PERSON2 mammu tinnava"
-echo"$PERSON2::ha nv tinnava"
+echo "$PERSON1::hello good morning"
+echo "$PERSON2::hello $PERSON2 good morning"
+echo "$PERSON1::$PERSON2 mammu tinnava"
+echo "$PERSON2::ha nv tinnava"
